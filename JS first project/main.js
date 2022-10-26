57c2fe402c512323abd4a5fd101f256896d32089
Nigar-Mahammadova <114868440+Nigar-Mahammadova@users.noreply.github.com>
2022-10-26 23:02:13 +0400

    Add files via upload

---
+++ JS first project/main.js
@@ -0,0 +1,196 @@
+// First
+// const a=+prompt('Enter number A');
+// const b=+prompt('Enter number B');
+// console.log(a);
+// console.log(b);
+// const result1= a / b;
+// const result2= a % b;
+// console.log(result1);
+// console.log(result2);
+
+// Second
+// let a=+prompt('Enter number A');
+// let b=+prompt('Enter number B');
+// console.log(a);
+// console.log(b);
+// let c=a;
+// a=b;
+// b=c;
+// console.log('after swapping is ${a}');
+// console.log('after swapping is ${b}');
+
+// Third
+// const a=+prompt("enter first number A");
+// const b=+prompt("enter second number B");
+// const c=+prompt("enter third number C");
+// const x=(c-b)/a;
+// console.log(x);
+
+// Fourth
+// const a=+prompt('enter first number A');
+// const b=+propmt('enter second number B');
+// let result='';
+// console.log(a);
+// console.log(b);
+// if(a>b){
+//  result=a+b;
+// }
+// else if(a==b){
+// result=a*b;    
+// }
+// else {
+// result=a-b;
+// }
+// console.log('result is ${result}');
+
+// Fifth
+// const a=+prompt('enter number A');
+// const b=+propmt('enter number B');
+// const c=+prompt('enter number C');
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// let Discriminant= b*b-4*a*c;
+// let root1, root2;
+// if(Discriminat>0){
+// root1=(-b+ Math.sqrt(Discriminant)) / (2*a);
+//  root2=(-b- Math.sqrt(Discriminant)) / (2*a);
+// }
+// else if(Discriminant==0)
+// {
+// root1=root2=-b/2*a;
+// }
+// else{
+// root1=root2='roots arent real number';
+// }
+// console.log('our roots: ${root1} ${root2} ')
+
+// Sixth
+ const a=+prompt('enter number');
+ console.log(a);
+ let.result1;
+ let.result2;
+ let.r=a%10;
+ let f=a-r;
+
+ if(a==10){
+    result1='ten';
+    result2='';
+ }
+ else if(a==11){
+    result1='eleven';
+    result2='';
+ }
+ else if(a==12){
+    result1='twelve';
+    result2='';
+ }
+ else if(a==13){
+    result1='thirteen';
+    result2='';
+ }
+else if(a==14){
+    result1='fourteen';
+    result2='';
+}
+else if(a==15){
+    result1='fifteen';
+    result2='';
+}
+else if(a==16){
+    result1='sixteen';
+    result2='';
+}
+else if(a==17){
+    result1='seventeen';
+    result2='';
+}
+else if(a==18){
+    result1='eighteen';
+    result2='';
+}
+else if(a==19){
+    result1='ninteen';
+    result2='';
+}else{
+    if(f==20){
+        result1='twenty';
+    }
+    else if(f==30){
+        result1='thirty';
+    }    
+else if(f==40){
+    result1='forty';
+}
+else if(f==50){
+    result1='fifty';
+}
+else if(f==60){
+    result1='sixty';
+}
+else if(f==70){
+    result1='seventy';
+}
+else if(f==80){
+    result1='eighty';
+}
+else if(f==90){
+    result1='ninty';
+}
+if(r==1){
+    result2='one';
+}
+else if(r==2){
+    result2='two';
+}
+else if(r==3){
+    result2='three';
+}
+else if(r==4){
+    result2='four';
+}
+else if(r==5){
+    result2='five';
+}
+else if(r==6){
+    result2='six';
+}
+else if(r==7){
+    result2='seven';
+}
+else if(r==8){
+    result2='eight';
+}
+else if(r==9){
+    result2='nine';
+}
+}
+
+console.log('number is: ${result1} $result2)')
+
+
+// Seventh
+// const x=+prompt('enter number X');
+// const y=+prompt('enter number Y');
+// const r=+prompt('enter number R');
+// let result;
+// console.log(x);
+// console.log(y);
+// console.log(r);
+// if(x**2 + y**2>r**2){
+// result=('in the inside');
+// }
+// else if(x**2 + y**2==r**2)
+// {
+// result=('in the center');
+// }
+// else{
+// result=('in the outside')'
+// }
+// console.log('${result}');
+
+
+
+
+
+
